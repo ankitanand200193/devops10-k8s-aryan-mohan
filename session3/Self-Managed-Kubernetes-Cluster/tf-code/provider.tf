c3228c303feb1c443e823fa409f8381ac4abc729
Aryan Mohan <aryan.mohan60@gmail.com>
2025-06-09 18:47:02 +0530

    Session3 and Session4 notes and manifests added

---
+++ session3/Self-Managed-Kubernetes-Cluster/tf-code/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region  = var.region
+  profile = "hvd"
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
